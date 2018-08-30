1d7d16fe215764f21a3f47861effb80d93ed671f
willson61 <31348545+willson61@users.noreply.github.com>
2018-08-30 07:32:59 -0600

    Primer commit

---
+++ app/src/main/java/com/example/sthephan/ejemplo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sthephan.ejemplo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.txtName)
+    EditText txtName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
